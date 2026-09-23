46635b2c854dbb6b90731efb4f18fde9cb7ef253
alimkulov7 <alimkulovakbar7@gmail.com>
2026-09-23 21:36:44 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log("PORT",process.env.PORT);
-console.log("MONGO_URL",process.env.MONGO_URL);
+import mongoose from 'mongoose';
+import app from "./app"
+
+mongoose.connect(process.env.MONGO_URL as string, {}).then(data => {
+    console.log("Mongodb connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+        console.log(`Server running successfully on port: ${PORT}`);
+    });
+})
+.catch(err => {
+    console.log("ERROR on connection mongodb",err);
+})
--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,13 @@
-console.log("hellooo");
+// M-TASK
+function getSquareNumbers(numbers: number[]) {
+    for (let number of numbers) {
+        const result = {
+            number: number,
+            square: number * number
+        };
+
+        console.log(result);
+    }
+}
+
+getSquareNumbers([45,5,6]);
