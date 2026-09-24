be0ed074ba503274de04906511c8e675ebc789dc
alimkulov7 <alimkulovakbar7@gmail.com>
2026-09-24 23:20:56 +0900

    fix: fixed mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
-import dotenv from 'dotenv';
+import dns from "dns";     ///  YANGI LINE
+import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from 'mongoose';
-import app from "./app"
+import mongoose from "mongoose";
+import app from "./app";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);   //=>  YANGI LINE noutbuk dnsi bilan muoomo bolganiligi uchun mongodb error kelib chiqdi
 
 mongoose.connect(process.env.MONGO_URL as string, {}).then(data => {
     console.log("Mongodb connection succeed");
+
     const PORT = process.env.PORT ?? 3003;
+
     app.listen(PORT, function () {
         console.log(`Server running successfully on port: ${PORT}`);
     });
 })
 .catch(err => {
-    console.log("ERROR on connection mongodb",err);
-})
+    console.log("ERROR on connection mongodb", err);
+});
